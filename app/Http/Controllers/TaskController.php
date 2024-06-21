eb424eca1818ea226b70d5fdbac1f2920e951280
DayronGi <dayron_giron@hotmail.com>
2024-06-21 08:20:27 -0500

    correciones en la relacion

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -69,4 +69,64 @@ class TaskController extends Controller
             return response()->json(['error' => 'Error al intentar guardar task.', 'exception' => $e->getMessage()], 500);
         }
     }
+
+    public function view($task_id)
+    {
+        $task = Task::with('meeting')->findOrFail($task_id);
+        return response()->json(['task' => $task]);
+    }
+
+    public function update(Request $request, $task_id)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'start_date' => 'required|date',
+            'estimated_time' => 'nullable|integer',
+            'units' => 'nullable|string',
+            'type_id' => 'required|integer',
+            'task_description' => 'required|string',
+            'assigned_to' => 'nullable|integer',
+            'department_id' => 'required|integer',
+            'observations' => 'nullable|string',
+            'created_by' => 'required|integer',
+            'creation_date' => 'required|date',
+            'reviewed_by' => 'nullable|integer',
+            'review_date' => 'nullable|date',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        try {
+            $task = Task::findORfail($task_id);
+            $task->update([
+                'start_date' => $request->start_date,
+                'estimated_time' => $request->estimated_time,
+                'units' => $request->units,
+                'type_id' => $request->type_id,
+                'task_description' => $request->task_description,
+                'assigned_to' => $request->assigned_to,
+                'department_id' => $request->department_id,
+                'observations' => $request->observations,
+                'created_by' => $request->created_by,
+                'creation_date' => $request->creation_date,
+                'reviewed_by' => $request->reviewed_by,
+                'review_date' => $request->review_date,
+            ]);
+            return response()->json(['message' => 'Task actualizado correctamente.']);
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return response()->json(['error' => 'Error al intentar actualizar task.', 'exception' => $e->getMessage()], 500);
+        }
+    }
+
+    public function delete($obligation_id)
+    {
+        $obligation = Task::findORfail($obligation_id);
+        $obligation->update([
+            'status' => 1
+        ]);
+        return response()->json(['message' => 'Task eliminado correctamente.']);
+    }
 }
