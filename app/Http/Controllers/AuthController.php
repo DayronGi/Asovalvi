c0dcbc87d44592e1dd3f4a4c8e7ede9b244afc54
DayronGi <dayron_giron@hotmail.com>
2024-06-25 11:07:03 -0500

    authcontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,22 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        return response()->json(['message' => 'register ok']);
+        $request->validate([
+            'name' => 'required|unique:users',
+            'password' => 'required'
+        ]);
+
+        $user  = new User();
+        $user->name = $request->name;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return response()->json(['message' => 'registrado correctamente']);
     }
 
     public function login(Request $request)
     {
-        return response()->json(['message' => 'login ok']);
+        $credentials = $request->validate([
+            'name' => ['required'],
+            'password' => ['required']
+        ]);
+
+        if (Auth::attempt($credentials))
+        {
+            $user = Auth::user();
+            $token = $user->createToken('token')->plainTextToken;
+            $cookie = cookie('cookie_token', $token, 60 * 24);
+            return response(['token' => $token], Response::HTTP_OK)->withoutCookie($cookie);
+        } else {
+            return response(['message' => 'Credenciales invalidas'], Response::HTTP_UNAUTHORIZED);
+        }
     }
 
-    public function logout(Request $request)
+    public function logout()
     {
-        return response()->json(['message' => 'logout ok']);
+        $cookie = Cookie::forget('cookie_token');
+        return response(['message' => 'Cierre de session correcto'])->withCookie($cookie);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MeetingController;
 use App\Http\Controllers\ObligationController;
 use App\Http\Controllers\StateController;
 use App\Http\Controllers\TaskController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::controller(TaskController::class)->group(function() {
+Route::controller(TaskController::class)->middleware('auth:sanctum')->group(function() {
     Route::get('/tasks', 'list');
     Route::post('/tasks', 'store');
     Route::get('/tasks/{task_id}', 'view');
@@ -19,7 +15,7 @@ Route::controller(TaskController::class)->group(function() {
     Route::put('/tasks/{task_id}/delete', 'delete');
 });
 
-Route::controller(MeetingController::class)->group(function() {
+Route::controller(MeetingController::class)->middleware('auth:sanctum')->group(function() {
     Route::get('/meetings', 'list');
     Route::post('/meetings', 'store');
     Route::get('/meetings/{meeting_id}', 'view');
@@ -27,7 +23,7 @@ Route::controller(MeetingController::class)->group(function() {
     Route::put('/meetings/{meeting_id}/delete', 'delete');
 });
 
-Route::controller(ObligationController::class)->group(function() {
+Route::controller(ObligationController::class)->middleware('auth:sanctum')->group(function() {
     Route::get('/obligations', 'list');
     Route::post('/obligations', 'store');
     Route::get('/obligations/{obligation_id}', 'view');
@@ -35,8 +31,12 @@ Route::controller(ObligationController::class)->group(function() {
     Route::put('/obligations/{obligation_id}/delete', 'delete');
 });
 
-Route::controller(StateController::class)->group(function() {
+Route::controller(StateController::class)->middleware('auth:sanctum')->group(function() {
     Route::get('/status/tasks', 'tasks');
     Route::get('/status/meetings', 'meetings');
     Route::get('/status/obligations', 'obligations');
 });
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
